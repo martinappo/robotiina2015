b7a294bb3017346c817f305bc5372ecb0d8ebe00
Gustav Amer <gustav.amer@rgrd.ee>
2015-08-26 20:32:18 +0300

    Moved frame center coordinates from DistranceCalculator class variables to getDistance() method variable.

--- DistanceCalculator.h
+++ DistanceCalculator.h
@@ -3,12 +3,10 @@
 
 class DistanceCalculator{
 public:
-	DistanceCalculator(int centerX, int centerY);
-	int getDistance(int x, int y);
+	DistanceCalculator();
+	int getDistance(int centerX, int centerY, int x, int y);
 
 private:
-	int mCenterX;
-	int mCenterY;
 	void loadConf();
 	int realDistances[DistanceCalibrator::CONF_SIZE];
 	double references[DistanceCalibrator::CONF_SIZE];
