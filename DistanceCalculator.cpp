c0cf2f5cd68f7196c4003e5a7bd5cc29a565e772
Andres <andres.viikmaa@gmail.com>
2015-09-15 12:30:11 +0300

    smooth distance calculator

--- DistanceCalculator.cpp
+++ DistanceCalculator.cpp
@@ -28,12 +28,20 @@ double DistanceCalculator::getDistance(int centerX, int centerY, int x, int y){
 	double dist = DistanceCalibrator::calculateDistance(centerX, centerY, x, y);
 	double minDif = INT_MAX;
 	int index = 0;
-	for (int i = 0; i < DistanceCalibrator::CONF_SIZE; i++){
+	for (int i = 1; i < DistanceCalibrator::CONF_SIZE; i++){
+		if (references[i] > dist) {
+			double how_far_between_two_steps = (dist - references[i - 1]) / (references[i] - references[i - 1]);
+			double realdist = realDistances[i - 1] + (realDistances[i] - realDistances[i - 1])*how_far_between_two_steps;
+			std::cout << "distance " << dist << " -> " << realdist << std::endl;
+			return 4*realdist;
+		}
+		/*
 		double dif = std::abs(references[i] - dist);
 		if (dif < minDif){
 			index = i;
 			minDif = dif;
 		}
+		*/
 	}
 	return realDistances[index]*4;
 //#error get rid of this magic number
