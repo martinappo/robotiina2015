bd65f359154bfbd215afe4b0c0afc68a3ed76d9c
Andres <andres.viikmaa@gmail.com>
2015-09-15 12:05:24 +0300

    nice background to field

--- SoccerField.cpp
+++ SoccerField.cpp
@@ -4,6 +4,9 @@
 
 SoccerField::SoccerField(IDisplay *pDisplay, cv::Size frameSize) :m_pDisplay(pDisplay)
 {
+	green = cv::imread("field.png", CV_LOAD_IMAGE_COLOR);   // Read the file
+	field = cv::Mat(green.size(), CV_8UC3, cv::Scalar::all(245));
+	c = green.size() / 2;
 	this->self.setFrameSize(frameSize);
 	initBalls(frameSize);
 	Start();
@@ -32,39 +35,46 @@ void SoccerField::Run(){
 		green.copyTo(field);
 
 
-		cv::circle(field, self.fieldCoords + cv::Point(250, 150), 14, cv::Scalar(133, 33, 55), 4);
+		cv::circle(field, self.fieldCoords + c, 14, cv::Scalar(133, 33, 55), 4);
+		cv::line(field, self.fieldCoords + c,
+			cv::Point(0, 0) - cv::Point(40.0*sin(self.robotAngle / 360 * TAU),40*cos(self.robotAngle / 360 * TAU))
+			+ self.fieldCoords + c
+			, cv::Scalar(133, 33, 55), 3);
+
+
 
 		for (int i = 0; i < NUMBER_OF_BALLS; i++) {
 			BallPosition &_ball = balls[i];
-			cv::circle(field, _ball.fieldCoords + cv::Point(250, 150), 7, cv::Scalar(48, 154, 236), -1);
+			cv::circle(field, _ball.fieldCoords + c, 7, cv::Scalar(48, 154, 236), -1);
 			_ball.setIsUpdated(false);
 		}
 
 		if (blueGate.getDistance() > 0) {
-			cv::circle(field, blueGate.fieldCoords + cv::Point(250,150), 14, cv::Scalar(236, 137, 48), 7);
-			cv::circle(field, blueGate.fieldCoords + cv::Point(250, 150), blueGate.polarMetricCoords.x, cv::Scalar(236, 137, 48), 2);
-			cv::line(field, blueGate.fieldCoords + cv::Point(250, 150),
-				cv::Point(0,0)- cv::Point((double)blueGate.polarMetricCoords.x*sin(blueGate.polarMetricCoords.y/360*TAU),
-				(double)blueGate.polarMetricCoords.x*cos(blueGate.polarMetricCoords.y / 360 * TAU)
-				) + blueGate.fieldCoords + cv::Point(250, 150)
-				, cv::Scalar(236, 137, 48));
+			cv::circle(field, blueGate.fieldCoords + c, 14, cv::Scalar(236, 137, 48), 7);
+			cv::circle(field, blueGate.fieldCoords + c, blueGate.polarMetricCoords.x, cv::Scalar(236, 137, 48), 2);
+			cv::line(field, blueGate.fieldCoords + c,
+				cv::Point(0,0)- cv::Point(blueGate.polarMetricCoords.x*sin(blueGate.polarMetricCoords.y/360*TAU),
+										blueGate.polarMetricCoords.x*cos(blueGate.polarMetricCoords.y / 360 * TAU)
+				) + blueGate.fieldCoords + c
+				, cv::Scalar(236, 137, 48),3);
 
 		}
+		/*
 		std::cout << blueGate.polarMetricCoords.x << ", " << blueGate.polarMetricCoords.y << ", " 
 			<< blueGate.polarMetricCoords.x*cos(blueGate.polarMetricCoords.y / 360 * TAU) << ", "
 			<< blueGate.polarMetricCoords.x*sin(blueGate.polarMetricCoords.y / 360 * TAU) << " <==> ";
 		std::cout << yellowGate.polarMetricCoords.x << ", " << yellowGate.polarMetricCoords.y << ", " 
 			<< yellowGate.polarMetricCoords.x*cos(yellowGate.polarMetricCoords.y / 360 * TAU) << ", "
 			<< yellowGate.polarMetricCoords.x*sin(yellowGate.polarMetricCoords.y / 360 * TAU) << std::endl;
-
+		*/
 		if (yellowGate.getDistance() > 0) {
-			cv::circle(field, yellowGate.fieldCoords + cv::Point(250, 150), 14, cv::Scalar(61, 255, 244), 7);
-			cv::circle(field, yellowGate.fieldCoords + cv::Point(250, 150), yellowGate.polarMetricCoords.x, cv::Scalar(61, 255, 244), 2);
+			cv::circle(field, yellowGate.fieldCoords + c, 14, cv::Scalar(61, 255, 244), 7);
+			cv::circle(field, yellowGate.fieldCoords + c, yellowGate.polarMetricCoords.x, cv::Scalar(61, 255, 244), 2);
 			
-			cv::line(field, yellowGate.fieldCoords + cv::Point(250, 150),
+			cv::line(field, yellowGate.fieldCoords + c,
 				cv::Point(0, 0) - cv::Point(yellowGate.polarMetricCoords.x*cos(yellowGate.polarMetricCoords.y / 360 * TAU),
 				yellowGate.polarMetricCoords.x*sin(yellowGate.polarMetricCoords.y / 360 * TAU)
-				) + yellowGate.fieldCoords + cv::Point(250, 150)
+				) + yellowGate.fieldCoords + c
 				, cv::Scalar(61, 255, 244), 3);
 			
 		}
