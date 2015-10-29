43301b610376c4c86b2eeb80e9f65dca30419dfe
andresviikmaa <andres.viikmaa@gmail.com>
2015-10-29 21:19:15 +0200

    window size smaller

--- robotiina_simulator_slave.bat
+++ robotiina_simulator_slave.bat
@@ -1,2 +1,2 @@
 PATH=C:\opencv3\build\x86\vc12\bin;C:\boost_1_56_0\lib32-msvc-12.0;%PATH%
-Debug\robotiina.exe --camera simulator --skip-ports --app-size 1000x900 --play-mode opponent
+Debug\robotiina.exe --camera simulator --skip-ports --app-size 1000x720 --play-mode opponent
