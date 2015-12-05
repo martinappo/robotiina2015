c9ea3d53f1af2af28932953986e27751568d4807
Marek Pagel <pagel.marek@gmail.com>
2015-12-05 23:06:25 +0200

    First steps to obstruction detection

--- FrontCameraVision.cpp
+++ FrontCameraVision.cpp
@@ -121,20 +121,23 @@ void FrontCameraVision::Run() {
 		/* this is done here, because finding contures	  */
 		/* corrupts thresholded imagees					  */
 		/**************************************************/
-		if (false && gateObstructionDetectionEnabled) {
+		if (gateObstructionDetectionEnabled) {
 			cv::Mat selected(frameSize.height, frameSize.width, CV_8U, cv::Scalar::all(0));
 			cv::Mat mask(frameSize.height, frameSize.width, CV_8U, cv::Scalar::all(0));
 			cv::Mat	tmp(frameSize.height, frameSize.width, CV_8U, cv::Scalar::all(0));
 			//cv::line(mask, cv::Point(frameSize.width / 2, 200), cv::Point(frameSize.width / 2 - 40, frameSize.height - 100), cv::Scalar(255, 255, 255), 40);
 			//cv::line(mask, cv::Point(frameSize.width / 2, 200), cv::Point(frameSize.width / 2 + 40, frameSize.height - 100), cv::Scalar(255, 255, 255), 40);
 			std::vector<cv::Point2i> triangle;
-			triangle.push_back(cv::Point(frameSize.width / 2, 50));
-			triangle.push_back(cv::Point(frameSize.width / 2 - 80, frameSize.height - 100));
-			triangle.push_back(cv::Point(frameSize.width / 2 + 80, frameSize.height - 100));
+			int halfWidth = frameSize.width / 2;
+			int halfHeight = frameSize.height / 2;
+			triangle.push_back(cv::Point(halfWidth, halfHeight- 45));
+			triangle.push_back(cv::Point(halfWidth - 80, halfHeight / 2 -45));
+			triangle.push_back(cv::Point(halfWidth + 80, halfHeight / 2 -45));
 			cv::fillConvexPoly(mask, triangle, cv::Scalar::all(255));
 			tmp = 255 - (thresholdedImages[INNER_BORDER] + thresholdedImages[OUTER_BORDER] + thresholdedImages[FIELD]);
 			tmp.copyTo(selected, mask); // perhaps use field and inner border
-			thresholdedImages[SIGHT_MASK] = selected;
+			thresholdedImages[SIGHT_MASK] = selected; 
+			cv::polylines(frameBGR, triangle, true, (255, 255, 255));
 			//sightObstructed = countNonZero(selected) > 10;
 		}
 
@@ -469,8 +472,7 @@ void FrontCameraVision::Run() {
 		if (gateObstructionDetectionEnabled) {
 			// step 3.2
 			int count = countNonZero(thresholdedImages[SIGHT_MASK]);
-			std::ostringstream osstr;
-			osstr << "nonzero :" << count;
+			std::cout << "obsCount: " << count << std::endl;
 			m_pState->gateObstructed = count > 900;
 			//cv::putText(thresholdedImages[SIGHT_MASK], osstr.str(), cv::Point(20, 20), cv::FONT_HERSHEY_DUPLEX, 0.5, cv::Scalar(255, 255, 255));
 			//cv::imshow("mmm", thresholdedImages[SIGHT_MASK]);
