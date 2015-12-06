75ee0fee192c77cb22cccc23318096394dba1915
Andres Viikmaa <andres.viikmaa@gmail.com>
2015-12-06 05:01:05 +0200

    borked

--- StateMachine.cpp
+++ StateMachine.cpp
@@ -62,14 +62,18 @@ bool DriveInstruction::driveToTargetWithAngle(const ObjectPosition &target, Spee
 
 
 const BallPosition &DriveInstruction::getClosestBall(bool includeHeading, bool reset){
-	/*if (includeHeading){
+	if (includeHeading){
 		BallPosition & closestBall = m_pFieldState->balls[0];
-		for (BallPosition &ball : m_pFieldState->balls){
-			if ((fabs(ball.getHeading()) + ball.getDistance()) < (fabs(closestBall.getHeading()) + closestBall.getDistance()))
-				closestBall = ball;
+		double dist = (fabs(closestBall.getHeading()*1.5) + closestBall.getDistance());
+		int index = 0;
+		for(int i = 0; i < m_pFieldState->balls.size(); i++){
+			if ((fabs(m_pFieldState->balls[i].getHeading()) + m_pFieldState->balls[i].getDistance()) < dist){
+				index = i;
+				dist = (fabs(m_pFieldState->balls[i].getHeading()) + m_pFieldState->balls[i].getDistance());
+			}
 		}
-		return closestBall;
-	}*/
+		return m_pFieldState->balls[index];
+	}
 	return  m_pFieldState->balls.getClosest();
 }
 
